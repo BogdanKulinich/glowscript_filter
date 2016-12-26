86d8a40eb8167a33649c99988a74d8d9edce50f7
Bogdan Kulinich <kulinich.b@gmail.com>
2016-12-26 07:21:56 +0200

    Add files via upload

---
+++ glowscript/version.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * GlowScript filter version information
+ *
+ * @package    filter
+ * @subpackage glowscript
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$plugin->version  = 1482729561;
+$plugin->component= 'filter_glowscript';
