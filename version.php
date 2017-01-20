b4d420a3318b718e1cc0c4284f371d2f28041969
Bogdan Kulinich <kulinich.b@gmail.com>
2017-01-20 12:44:08 +0200

    Beta v1.0 update

--- version.php
+++ version.php
@@ -3,12 +3,15 @@
 /**
  * GlowScript filter version information
  *
- * @package    filter
- * @subpackage glowscript
+ * @package    filter_glowscript
+ * @copyright 2017, Bogdan Kulinich kulinich.b@gmail.com
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version  = 1482729561;
+$plugin->version  = 2017012000;
+$plugin->requires = 2014051200;
+$plugin->maturity = MATURITY_BETA;
+$plugin->release = 'v1.0';
 $plugin->component= 'filter_glowscript';
