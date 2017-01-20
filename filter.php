f1407e7218a8ecdc0cf70006d8de041cbc051baf
Bogdan Kulinich <kulinich.b@gmail.com>
2017-01-20 12:43:25 +0200

    Beta v1.0 update

--- filter.php
+++ filter.php
@@ -1,41 +1,36 @@
 <?php
-class filter_glowscript extends moodle_text_filter {
-    public function filter($text, array $options = array()) {
-		
-	// Required scripts from official GlowScript website
-	$scripts = '
-	 <script type="text/javascript"
-     src="http://www.glowscript.org/lib/jquery/2.1/jquery.min.js">
-     </script>
-     <script type="text/javascript"
-     src="http://www.glowscript.org/lib/jquery/2.1/jquery-ui.custom.min.js">
-     </script>
-     <script type="text/javascript"
-     src="http://www.glowscript.org/package/glow.2.1.min.js">
-     </script>
-	';
-		
-	$addedScript = false;
-	// Creating unique value to use it as id attribute for <div> tag which we will use as a container for script results
-    $id = uniqid();
-	// Put container declaration as a string in var	
-    $target = "window.__context = {glowscript_container: $('#" . $id . "').removeAttr('id')};";
-		
-	// Checking if we have open and close GS filter 	
-	while(strpos($text,"[GS") !== false and strpos($text,"GS]") !== false){
-		// If we didn't add required scripts	
-		if (!$addedScript)
-		{
-			$addedScript = true;
-			echo $scripts;	
-		}
-		// Replacing text
-		$replace = "<div id='" . $id . "'></div><script type='text/javascript'>" . $target;
-		$text = preg_replace("/[[]GS/", $replace, $text, 1);
-		$text = preg_replace("/GS[]]/", "</script>", $text, 1);
-		}
 
-    return $text;
+class filter_glowscript extends moodle_text_filter
+{
+    public function filter($text, array $options = array())
+    {
+        $initJS = file_get_contents(__DIR__.'/init.js');
+        $scripts = "
+        <script type=\"text/javascript\" src=\"https://cdn.mathjax.org/mathjax/latest/MathJax.js?config=TeX-AMS_HTML\"></script>
+        <script type=\"text/javascript\" src=\"http://www.glowscript.org/lib/jquery/2.1/jquery.min.js\"></script>
+        <script type=\"text/javascript\" src=\"http://www.glowscript.org/lib/jquery/2.1/jquery-ui.custom.min.js\"></script>
+        <script type=\"text/javascript\" src=\"http://www.glowscript.org/package/glow.2.1.min.js\"></script>
+        <script type=\"text/javascript\" src=\"http://www.glowscript.org/package/RSrun.2.1.min.js\"></script>
+	";
+        echo '<script id="initJS" type="text/javascript">' . $initJS . '</script>';
+        $addedScript = false;
+        while (strpos($text, "[GS") !== false and strpos($text, "GS]") !== false) {
+            if (!$addedScript) {
+                $addedScript = true;
+                echo $scripts;
+            }
+            $id = uniqid();
+            $partScript = "
+        <script type='text/javascript'>
+            runJavaScript('#{$id}');
+        </script>
+        ";
+            $replaceOpen = "<glowscript id='{$id}'><textarea style='display: none;'>";
+            $replaceClose = "</textarea><div class='GS_figure'></div><div class='GS_errors'></div></glowscript>";
+            $text = preg_replace("/[[]GS/", $replaceOpen, $text, 1);
+            $text = preg_replace("/GS[]]/", $replaceClose . $partScript, $text, 1);
+        }
+        return $text;
     }
 }
 ?>
